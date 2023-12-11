abf3b1d522f526b5db3b26f1b2d92fee2331c463
WerderRZ <rzaxel@gmail.com>
2023-12-11 17:42:57 +0300

    PA-3 Правки 3

--- src/main/java/com/werdersoft/personapi/util/Utils.java
+++ src/main/java/com/werdersoft/personapi/util/Utils.java
@@ -22,18 +22,4 @@ public class Utils {
         return op.orElseThrow(() -> ex);
     }
 
-    public static List<UUID> mapEntitiesToEntitiesIds(Set<? extends BaseEntity> entity)  {
-        if (entity != null) {
-            return entity.stream()
-                    .map(BaseEntity::getId)
-                    .toList();
-        } else {
-            return new ArrayList<UUID>();
-        }
-    }
-
-    public static <T extends BaseEntity> UUID mapEntityToEntityId(T entity) {
-        return (entity != null) ? entity.getId() : null;
-    }
-
 }
